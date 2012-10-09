770b4390a5d6e77020b29b3d0cd07eae5069e165
Jevgenij Tichonov <jenkus@mail.ru>
2012-10-09 14:31:38 +0300

    Vokiskai pasisveikinau ir biski pataisiau

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main ()
   cout<< "Hello World!"<<endl;
   cout<<"Sveikas pasauly!!!"<<endl;
   cout<<"Sveikas gyvas!!!"<<endl;
-  cout<<"Sveikas da karta!!!"<<endl;
+  cout<<"Sveikas da karta..."<<endl;
+  cout<<"Gutte abben!!!!"<<endl;
   return 0;
 }
